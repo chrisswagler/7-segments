7bfddf95d0774f0caa2fa18b7c7d9c6681298a65
chrisswagler <76789839+chrisswagler@users.noreply.github.com>
2020-12-31 21:07:32 -0500

    Update DE1SoCfpga.h

--- DE1SoCfpga.h
+++ DE1SoCfpga.h
@@ -1,4 +1,4 @@
-//Christopher Swagler 001328396
+//Christopher Swagler
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,17 +8,17 @@
 using namespace std;
 
 // Physical base address of FPGA Devices
-const unsigned int LW_BRIDGE_BASE 			= 0xFF200000;  // Base offset
+const unsigned int LW_BRIDGE_BASE 	= 0xFF200000;  // Base offset
 
 // Length of memory-mapped IO window
-const unsigned int LW_BRIDGE_SPAN 			= 0x00005000;  // Address map size
+const unsigned int LW_BRIDGE_SPAN 	= 0x00005000;  // Address map size
 
 // Cyclone V FPGA device addresses
-const unsigned int LEDR_BASE 						= 0x00000000;  // Leds offset
+const unsigned int LEDR_BASE 		= 0x00000000;  // Leds offset
 const unsigned int HEX3_HEX0_BASE       = 0x00000020;  // Data Reg1 offset
 const unsigned int HEX5_HEX4_BASE       = 0x00000030;  // Data Reg2 offset
-const unsigned int SW_BASE 							= 0x00000040;  // Switches offset
-const unsigned int KEY_BASE 						= 0x00000050;  // Push buttons offset
+const unsigned int SW_BASE 		= 0x00000040;  // Switches offset
+const unsigned int KEY_BASE 		= 0x00000050;  // Push buttons offset
 
 //class for main functions of the de1soc board from previous program
 class DE1SoCfpga
